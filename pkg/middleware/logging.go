290b2a5eed3b7400f05c7fbcd3127afb58d5950b
Aleksandr Samokhin <beetee.tanais@yandex.ru>
2025-10-23 12:47:36 +0300

    fix : Add test to logger

--- pkg/middleware/logging.go
+++ pkg/middleware/logging.go
@@ -7,52 +7,43 @@ import (
 	"go.uber.org/zap"
 )
 
-// responseWriter обёртка для захвата статуса и размера ответа
 type responseWriter struct {
 	http.ResponseWriter
 	statusCode int
 	size       int
 }
 
-// newResponseWriter создаёт обёртку над ResponseWriter
 func newResponseWriter(w http.ResponseWriter) *responseWriter {
 	return &responseWriter{
 		ResponseWriter: w,
-		statusCode:     http.StatusOK, // По умолчанию 200
+		statusCode:     http.StatusOK,
 		size:           0,
 	}
 }
 
-// WriteHeader перехватывает код статуса
 func (rw *responseWriter) WriteHeader(statusCode int) {
 	rw.statusCode = statusCode
 	rw.ResponseWriter.WriteHeader(statusCode)
 }
 
-// Write перехватывает размер ответа
 func (rw *responseWriter) Write(b []byte) (int, error) {
 	size, err := rw.ResponseWriter.Write(b)
 	rw.size += size
 	return size, err
 }
 
-// WithLogging добавляет логирование запросов и ответов
 func WithLogging(logger *zap.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Запоминаем время начала
+
 			start := time.Now()
 
-			// Оборачиваем ResponseWriter для перехвата данных
 			rw := newResponseWriter(w)
 
-			// Вызываем следующий handler
 			next.ServeHTTP(rw, r)
 
-			// Вычисляем длительность
 			duration := time.Since(start)
 
-			// Логируем запрос и ответ
 			logger.Info("HTTP request",
 				zap.String("method", r.Method),
 				zap.String("uri", r.RequestURI),
