2a0330ca4e1f809b340fef9a6aeb7f250296f2c2
Aleksandr Samokhin <beetee.tanais@yandex.ru>
2025-10-24 12:28:37 +0300

    fix : fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -42,11 +42,11 @@ func run() error {
 
 	router.Use(middleware.WithLogging(logger))
 
-	router.Post("/update/{type}/{name}/{value}", metricshandler.UpdateMetricHandler)
-	router.Get("/value/{type}/{name}", metricshandler.GetMetricHandler)
-
 	router.Post("/update", metricshandler.UpdateHandler)
 	router.Post("/value", metricshandler.GetMetric)
+
+	router.Post("/update/{type}/{name}/{value}", metricshandler.UpdateMetricHandler)
+	router.Get("/value/{type}/{name}", metricshandler.GetMetricHandler)
 
 	router.Get("/", metricshandler.ListMetricsHandler)
 
